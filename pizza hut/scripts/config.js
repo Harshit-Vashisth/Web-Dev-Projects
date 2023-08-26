8b3b12c9e7cbfe6d9b4b0c5687dad5dcdd98d794
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-08-26 12:36:05 +0530

    by harshit

--- pizza hut/scripts/config.js
+++ pizza hut/scripts/config.js
@@ -1,1 +1,2 @@
 const URL="https://gist.github.com/kshirsagarps/36fade16fa39202715656ef487aaf7b0"
+// ocnfig are never part iof the code they are always her we are just adding them as we are the basic level 
