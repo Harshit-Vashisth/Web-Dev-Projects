a0eff7b69f0d9a22503649128303873c9d91287d
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-08-26 15:39:54 +0530

    Update pizza-controller.js

--- pizza hut/scripts/controller/pizza-controller.js
+++ pizza hut/scripts/controller/pizza-controller.js
@@ -1,1 +1,13 @@
 // this the middle of api call and our applicatins
+class Pizza{
+    constructor (id,name,price,url,desc){
+        this.id=id;
+        this.name=name;
+        this.price=price;
+        this.url=url;
+        this.desc=desc;
+    }
+    // now it will be easy for us to fetch this particular data 
+    // javascript is object based not object oriented 
+    
+}
