d2b2e97378ac98badd2821dc9b40b8014a6c1b7f
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-08-27 10:17:36 +0530

    h

--- pizza hut/scripts/controller/pizza-controller.js
+++ pizza hut/scripts/controller/pizza-controller.js
@@ -10,6 +10,27 @@ import { getPizzas } from "../services/pizza-operations.js";
 async function printPizzas()
 {
     const allPizzas=await getPizzas();
+    const div=document.getElementById('pizza-output');
     console.log('All Pizza',allPizzas);
+    for(var pizza of allPizzas){
+        const card=createCard(pizza);
+        div.appendChild(card);
+    }
 }
+
 printPizzas();
+
+function createCard(pizza){
+    // yaha humare card banege aur div me ek ek karke add
+    
+   /*
+   <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+   */
+}
